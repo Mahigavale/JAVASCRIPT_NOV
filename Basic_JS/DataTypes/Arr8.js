cebf867a8d08ba61b456b07fa0f435789055b491
Mahigavale <maheshgavale07@gmail.com>
2025-12-02 12:17:50 +0530

    Array Methods:

--- Basic_JS/DataTypes/Arr8.js
+++ Basic_JS/DataTypes/Arr8.js
@@ -1,9 +1,23 @@
 
-// let arr=["mango","apple","banana"]
 
-   
+/**
+ *   Splice()=>Modifying method.
+ * Elements remove ,Elements Add.
+ * Splice(a,b,c);
+ */
 
-// arr.splice(1,1,"add")
 
-//     console.log(arr);
-    
+let arr2=['mango','apple','banana'];
+          //0         1       2
+
+
+
+          // 'mango', 'berry', 'banana','jackfruit'
+
+          arr2.splice(1,1,'berry');
+          console.log(arr2);
+
+          arr2.splice(3,0,'jackfruit');
+          console.log(arr2);
+          
+          
---
+++ Basic_JS/DataTypes/Arr9.js
@@ -0,0 +1,31 @@
+
+
+
+let arr=[10,12,13,15,17,18,21];
+
+//v ->abc<-b
+
+
+
+// function check(i)
+// {
+//     return i>10;
+// }
+
+
+
+// console.log(arr.findLast(check));
+
+//  console.log(
+//   arr.findLastIndex(check));
+
+
+
+console.log(arr);
+
+  arr.fill('aekdnkednke',1,4);  //1,2,3 => 'a','a','a'
+  console.log(arr);
+  /**
+   * Fill is also a modifying method.
+   */
+  
